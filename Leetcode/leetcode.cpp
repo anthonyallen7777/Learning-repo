679d239ab6fbd19b883c5c69920502a296cb8f3f
anthonyallengithub <41088309+anthonyallengithub@users.noreply.github.com>
2022-06-03 11:58:32 -0500

    linkedlists

--- Leetcode/leetcode.cpp
+++ Leetcode/leetcode.cpp
@@ -1,7 +1,5 @@
-#include <iostream>
-#include <list>
-using namespace std;
 
+//Definition for singly-linked list.
 struct ListNode {
     int val;
     ListNode *next;
@@ -10,29 +8,11 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-void printList(ListNode* n)
-{
-    while (n != NULL) {
-        cout << n->val << " ";
-        n = n->next;
-    }
-}
 
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* p1 = list1;
-        ListNode* p2 = list2;
-        return list1;
+
     }
 };
 
-int main ()
-{
-    ListNode* l1 = {0};
-    ListNode* l2 = new ListNode();
-    l2 = { 0 };
-    printList(l1);
-    //Solution class1;
-    //class1.mergeTwoLists(l1, l2);
-};
---
+++ MIT CS50/Data Structures/hashfunction.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct node
+{
+    int number;
+    struct node *left;
+    struct node *right;
+}
+node;
+
+int main(void)
+{
+
+}
