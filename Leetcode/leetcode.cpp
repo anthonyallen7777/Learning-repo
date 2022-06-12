6e084ac8e7247c44a2f31fc9aba56b4096f54b8e
anthonyallengithub <41088309+anthonyallengithub@users.noreply.github.com>
2022-06-12 03:17:29 -0500

    more problems

--- Leetcode/leetcode.cpp
+++ Leetcode/leetcode.cpp
@@ -3,6 +3,10 @@
 #include <algorithm>
 #include <vector>
 #include <string>
+#include <sstream>
+#include <iterator>
+#include <map>
+
 using namespace std;
 
 class Solution {
@@ -10,46 +14,66 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> r;
 
-        //sort the strings individually alphabetically
-        for (auto& i: strs) {
-            sort(i.begin(), i.end());
-        }
-        //now sort the vector by strings alphabetically
-        sort(strs.begin(), strs.end());
+        std::map<int, string> my_map;
         
-        //aet aet aet ant ant
-        for (int i=1;i<strs.size();i++) {
-            //once we hit the end of a group of string 
-            //of anagrams
-            if (strs[i-1] != strs[i]) {
-                //cout << "LAST: "<<  strs[i-1] << '\n';
+        for (auto itr = my_map.begin(); itr != my_map.end(); ++itr) {
+            vector<string> v;
+            vector<int> u;
+            for (auto x = my_map.begin(); x != my_map.end(); ++x) {
+                if (itr->second == x->second) {
+                    v.push_back(strs[x->first]);
+                    u.push_back(x->first);
+                }
             }
-            cout << strs[i] << '\n';
-            if (strs[i] != strs[i-1]) {
-                //cout << strs[i] << '\n';
-                //make a vector to store the current group
-                //vector<string> insV;
-                //insV.assign(strs.begin(),strs.begin()+i);
-                //r.insert(r.end(), insV);
-                //strs.erase(strs.begin(),strs.begin()+i);
-            }
+            r.insert(r.end(), v);
         }
 
-        cout << '\n';
         for (int i = 0; i < r.size(); i++) {
-        for (auto it = r[i].begin(); it != r[i].end(); it++)
-            cout << *it << " ";
-            cout << endl;
+            for (auto it = r[i].begin(); it != r[i].end(); it++)
+                cout << "[" << *it << "]" << " ";
+                cout << endl;
         }
 
-        vector<vector<string>> dummy;
-        return dummy;
+        /*
+        if (strs.size() > 1) {
+            //sort the strings individually alphabetically
+            for (auto& i: strs) {
+                sort(i.begin(), i.end());
+            }
+            //now sort the vector by strings alphabetically
+            sort(strs.begin(), strs.end());
+            int d = 1;
+            while (strs.size() > 0) {
+                if (strs[d-1] != strs[d]) {
+                    //cout << strs[d-1] << '\n';
+                    vector<string> v;
+                    v.assign(strs.begin(),strs.begin()+d);
+                    r.insert(r.end(), v);
+                    strs.erase(strs.begin(), strs.begin()+d);
+                    d = 1;
+                }
+                d += 1;
+            }
+        } else {
+            r.insert(r.end(), strs);
+        }
+
+        for (int i = 0; i < r.size(); i++) {
+            for (auto it = r[i].begin(); it != r[i].end(); it++)
+                cout << "[" << *it << "]" << " ";
+                cout << endl;
+        }
+        */
+
+        return r;
     }
 };
 
 int main ()
 {
   Solution class1; //  aet   aet   
-  vector<string> s = {"eat","tea","tan","ate","nat","bat"};
-  class1.groupAnagrams(s);
+  vector<string> s1 = {"eat","tea","tan","ate","nat","bat"};
+  vector<string> s2 = {""};
+
+  class1.groupAnagrams(s1);
 };
