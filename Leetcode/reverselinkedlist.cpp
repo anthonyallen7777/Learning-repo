d01cfe611fb7194bafe5853b49af850f4d934b97
anthonyallengithub <41088309+anthonyallengithub@users.noreply.github.com>
2022-06-18 04:28:58 -0500

    reviewing linkedlists

--- Leetcode/reverselinkedlist.cpp
+++ Leetcode/reverselinkedlist.cpp
@@ -1,4 +1,6 @@
-
+#include <iostream>
+#include <string>
+using namespace std;
 //Definition for singly-linked list.
 struct ListNode {
     int val;
@@ -8,12 +10,23 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+void printList(ListNode* n)
+{
+    while (n != NULL) {
+        cout << n->val << " ";
+        n = n->next;
+    }
+}
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
+        //NULL->
         ListNode* previous = nullptr;
+        //1->2->3->NULL
         ListNode* current = head;
         while (current != nullptr) {
+            //cout << current->val << '\n';
             ListNode* nextVal = current->next;
             current->next = previous;
             previous = current;
@@ -21,4 +34,29 @@ public:
         }
         return previous;
     }
+};
+
+int main ()
+{
+    Solution class1;
+
+    ListNode* list1 = NULL;
+    ListNode* second = NULL;
+    ListNode* third = NULL;
+
+    list1 = new ListNode();
+    second = new ListNode();
+    third = new ListNode();
+
+    list1->val = 1;
+    list1->next = second;
+    second->val = 2;
+    second->next = third;
+    third->val = 3;
+    third->next = NULL;
+    
+
+    ListNode* r = class1.reverseList(list1);
+
+    printList(r);
 };
