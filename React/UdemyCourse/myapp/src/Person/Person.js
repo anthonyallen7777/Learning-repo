f43689c07046124e8eb8c287d40b239be5a4e399
anthonyallengithub <41088309+anthonyallen7777@users.noreply.github.com>
2022-10-09 02:55:15 -0500

    learning

--- React/UdemyCourse/myapp/src/Person/Person.js
+++ React/UdemyCourse/myapp/src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const person = () => { //es6 function
-    return <p>I'm a Person and I am {Math.floor(Math.random()* 30)} years old!</p>
+const person = (props) => { //es6 function
+    return (
+        <div>
+            <p>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+        </div>
+    )
 };
 
 export default person;
