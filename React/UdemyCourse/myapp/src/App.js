5eb6de0b9ba50f34f9b44addbca8efbd0a060dfa
anthonyallengithub <41088309+anthonyallen7777@users.noreply.github.com>
2022-10-10 15:47:45 -0500

    main

--- React/UdemyCourse/myapp/src/App.js
+++ React/UdemyCourse/myapp/src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       { name: 'Taylor', age: 43 },
       { name: 'Woody', age: 65 }
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false
   }
 
   switchNameHandler = (newName) => { //es6 function
@@ -34,6 +35,13 @@ class App extends Component {
     });
   }
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow
+    });
+  }
+
   render() {
     const buttonStyle = {
       backgroundColor: 'white',
@@ -43,24 +51,28 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map(person => {
+            return <Person
+            name={person.name}
+            age={person.age} />
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         <button
         style={buttonStyle}
-        onClick={() => this.switchNameHandler('Jeff!!')}>Switch Name</button>
-        <Person
-        name={this.state.persons[0].name}
-        age={this.state.persons[0].age} />
-        <Person
-        name={this.state.persons[1].name}
-        age={this.state.persons[1].age}
-        click={this.switchNameHandler.bind(this, "Jefferey!")}
-        changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
-        <Person
-        name={this.state.persons[2].name}
-        age={this.state.persons[2].age} />
+        onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
