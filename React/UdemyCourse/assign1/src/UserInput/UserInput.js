559bd09f085a4301ab76de88c125a92e0a9e8376
anthonyallengithub <41088309+anthonyallen7777@users.noreply.github.com>
2022-10-10 09:03:58 -0500

    style assign1

--- React/UdemyCourse/assign1/src/UserInput/UserInput.js
+++ React/UdemyCourse/assign1/src/UserInput/UserInput.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
 const userInput = (props) => {
+	const style = {
+		width: '60%',
+		border: '1px solid black',
+		margin: '5px auto',
+		padding: '2px',
+	};
+	const pStyle = {
+		display: 'inline-block',
+		padding: '0 5px 0 0'
+	}
+
 	return (
 		<div>
-			<p>Username:</p>
+		<div style={style}>
+			<p style={pStyle}>Username:</p>
 			<input type="text" onChange={props.changeInput} value={props.username}></input>
-			<p>Password:</p>
+		</div>
+		<div style={style}>
+			<p style={pStyle}>Password:</p>
 			<input type="password" onChange={props.changeInput} value={props.password}></input>
+		</div>
 		</div>
 		);
 };
