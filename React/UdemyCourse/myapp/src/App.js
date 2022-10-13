eb9e1a3726deaa19af7de745bac8ad41e24ac2e2
anthonyallengithub <41088309+anthonyallen7777@users.noreply.github.com>
2022-10-12 19:38:54 -0500

    eject and css modules

--- React/UdemyCourse/myapp/src/App.js
+++ React/UdemyCourse/myapp/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -47,8 +47,8 @@ class App extends Component {
   }
 
   render() {
-
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -64,22 +64,24 @@ class App extends Component {
           })}
         </div>
       );
+
+      btnClass = classes.Red;
     }
 
-    const classes  = [];
+    const assignedClasses  = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red'); // classes = ['red']
+      assignedClasses.push(classes.red); // classes = ['red']
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold'); // classes ['red', 'bold']
+      assignedClasses.push(classes.bold); // classes ['red', 'bold']
     }
 
 
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
-          <button className='button'
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
+          <button className={btnClass}
           onClick={this.togglePersonsHandler}>Toggle Persons
           </button>
           {persons}
