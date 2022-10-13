a044b31676cebde09d75067ae59593f8793894e6
anthonyallengithub <41088309+anthonyallen7777@users.noreply.github.com>
2022-10-12 21:36:34 -0500

    debugging module done

--- React/UdemyCourse/myapp/src/Person/Person.js
+++ React/UdemyCourse/myapp/src/Person/Person.js
@@ -2,6 +2,11 @@ import React from 'react';
 import classes from './Person.css';
 
 const person = (props) => { //es6 function
+    const rnd = Math.random();
+        if (rnd > 0.7) {
+            throw new Error('Something went wrong');
+        }
+
     return (
         //<div className='Person'
         <div className={classes.Person}>
