af48c4b3831c15bc5685080d9c744df81fc6ae6f
anthonyallengithub <41088309+anthonyallen7777@users.noreply.github.com>
2022-10-14 03:26:46 -0500

    interceptors and defaults

--- React/UdemyCourse/http1/src/index.js
+++ React/UdemyCourse/http1/src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Context-Type'] = 'application/json';
+
+axios.interceptors.request.use(request => {
+  console.log(request);
+  // Edit request config
+  return request;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+  console.log(response);
+  return response;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+})
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
