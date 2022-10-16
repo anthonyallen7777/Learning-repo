ab9d45e7efd03c734a824ecc291f851f660b5a67
anthonyallengithub <41088309+anthonyallen7777@users.noreply.github.com>
2022-10-16 04:28:14 -0500

    routing

--- React/UdemyCourse/http1/src/components/Post/Post.js
+++ React/UdemyCourse/http1/src/components/Post/Post.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// import withRouter from '../../hoc/withRouter/withRouter';
+
 import './Post.css';
 
 const post = props => (
-  <article className="Post" onClick={props.clicked}>
+    <article className="Post" onClick={props.clicked}>
     <h1>{props.title}</h1>
     <div className="Info">
       <div className="Author">{props.author}</div>
@@ -17,4 +19,5 @@ post.propTypes = {
   author: PropTypes.string.isRequired
 };
 
+// export default withRouter(post);
 export default post;
--- React/UdemyCourse/http1/src/containers/Blog/Blog.js
+++ React/UdemyCourse/http1/src/containers/Blog/Blog.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
 import './Blog.css';
-import { Link, Routes, Route } from "react-router-dom";
+import { NavLink, Routes, Route } from "react-router-dom";
+
+import withRouter from '../../hoc/withRouter/withRouter';
 
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
-
+    componentDidMount() {
+        console.log(this.props);
+    }
 
     render() {
-
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">HOME</Link></li>
-                            <li><Link to={{
-                                pathname: '/new-post',
+                            <li><NavLink
+                            // className='my-active'
+                            // style={{
+                            //     color: '#FA923F',
+                            //     textDecoration: 'underline'
+                            // }}
+                            to="/" 
+                            exact="true"
+                            end>HOME</NavLink></li>
+                            <li><NavLink to={{
+                                pathname:'/new-post',
                                 //hash: '',
                                 //search: '?quick-submit=true'
-                            }}>NEW POST</Link></li>
+                            }}>NEW POST</NavLink></li>
                         </ul>
                     </nav>
                 </header>
@@ -34,4 +45,4 @@ class Blog extends Component {
     }
 }
 
-export default Blog;
+export default withRouter(Blog);
