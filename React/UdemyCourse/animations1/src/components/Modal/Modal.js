cf90855b87495ef0f5b1bb82eb7100c4a34190cf
anthonyallengithub <41088309+anthonyallen7777@users.noreply.github.com>
2022-10-30 10:36:30 -0500

    csstransitions more convenient

--- React/UdemyCourse/animations1/src/components/Modal/Modal.js
+++ React/UdemyCourse/animations1/src/components/Modal/Modal.js
@@ -11,23 +11,17 @@ const animationTiming = {
 
 const modal = (props) => {
     return (
-    <Transition
-    mountOnEnter
-    unmountOnExit
-    in={props.show}
-    timeout={animationTiming}>
-    {state => {
-        const cssClasses = ["Modal",
-        state === 'entering' ? 'ModalOpen'
-        : state === 'exiting' ? 'ModalClosed' : null]
-        return (
-        <div className={cssClasses.join(' ')}>
+    <CSSTransition
+        mountOnEnter
+        unmountOnExit
+        in={props.show}
+        timeout={animationTiming}
+        classNames="fade-slide">
+        <div className="Modal">
             <h1>A Modal</h1>
             <button className="Button" onClick={props.closed}>Dismiss</button>
         </div>
-        );
-    }}
-    </Transition>
+    </CSSTransition>
     );
 };
 
