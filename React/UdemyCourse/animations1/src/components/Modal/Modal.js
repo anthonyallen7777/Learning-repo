4623389a9b0d6e34b51390b1881374c09152ff8a
anthonyallengithub <41088309+anthonyallen7777@users.noreply.github.com>
2022-10-30 12:37:23 -0500

    customize css learnapp

--- React/UdemyCourse/animations1/src/components/Modal/Modal.js
+++ React/UdemyCourse/animations1/src/components/Modal/Modal.js
@@ -16,7 +16,16 @@ const modal = (props) => {
         unmountOnExit
         in={props.show}
         timeout={animationTiming}
-        classNames="fade-slide">
+        // classNames={{
+        //     enter: '',
+        //     enterActive: 'ModalOpen',
+        //     exit: '',
+        //     exitActive: 'ModalClosed',
+        //     appear: '',
+        //     appearActive: '',
+        // }}
+        classNames={'fade-slide'}
+        >
         <div className="Modal">
             <h1>A Modal</h1>
             <button className="Button" onClick={props.closed}>Dismiss</button>
