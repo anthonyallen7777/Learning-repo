461f585380d550eb66002d1ca3265485900e24b1
anthonyallengithub <41088309+anthonyallen7777@users.noreply.github.com>
2022-11-04 17:22:42 -0500

    fixes

--- React/UdemyCourse/learnapp/src/store/sagas/index.js
+++ React/UdemyCourse/learnapp/src/store/sagas/index.js
@@ -1,4 +1,4 @@
-import { takeEvery, all } from "redux-saga/effects";
+import { takeEvery, all, takeLatest } from "redux-saga/effects";
 
 import * as actionTypes from '../actions/actionTypes';
 import { initIngredientsSaga } from "./burgerBuilder";
@@ -19,6 +19,6 @@ export function* watchAuth() {
 }
 
 export function* watchOrder() {
-    yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
+    yield takeLatest(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
     yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga);
 }
