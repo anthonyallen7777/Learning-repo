b503f0ddf775e74e27dc9cefb095d53648015322
anthonyallengithub <41088309+anthonyallen7777@users.noreply.github.com>
2023-12-26 17:45:24 -0600

    learn

---
+++ How to Code - Simple Data/predicates.rkt
@@ -0,0 +1,26 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname predicates) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;(string-length 42)
+;(* (+ (string-length 42) 1) pi)
+
+;(number? 4)
+;(number? pi)
+;(number? #true)
+;(number? "fortytwo")
+
+; rational number is a real number which
+; can be in the form p/q where q is not
+; equal to zero
+; 1/2, 1/5, 3/4
+; 0/1, 0/2, 0/3
+
+;(rational? pi)
+
+;(exact? (sqrt 2))
+;(exact? 2.14)
+
+(define in ...)
+  
+
+;(sqrt -1)
